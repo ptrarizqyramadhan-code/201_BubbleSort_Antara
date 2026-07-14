7ac3458e4a383f169b15feef89ac2b5dbb4dea2c
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-07-14 15:22:50 +0700

    void input

--- Bubbleshort.cpp
+++ Bubbleshort.cpp
@@ -4,3 +4,8 @@ using namespace std;
 int a[20]; // deklarasi array a dengan ukuran 20 bertipe integer
 int n;     // deklarasi variabel n untuk menyimpan banyaknya elemen array
 
+void input() {
+    while (true) // looping while
+    {
+        cout << "Masukkan panjang array : ";
+        cin >> n;
