559a715977464ec492f180859e46fb06fe96470a
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-07-14 15:24:51 +0700

    void Bubbleshort

--- Bubbleshort.cpp
+++ Bubbleshort.cpp
@@ -41,3 +41,17 @@ void display() {
         cout << "Data ke - " << i << " : " << a[i] << endl; // output data array ke layar
     }
 }
+
+void BubbleSort() {
+
+    for (int pass = 1; pass < n; pass++) // looping dengan variabel pass dimulai dari 1 hingga n - 1
+    {
+        for (int j = 0; j < n - pass; j++) { // looping dengan j dimulai dari nol hingga n - 1 - pass
+            if (a[j] > a[j + 1]) { // jika nilai array indeks ke j lebih besar dari array indeks ke j+1
+                int temp = a[j];   // simpan nilai indeks ke j di variabel sementara bernama temp
+                a[j] = a[j + 1];   // assign nilai array indeks ke j+1 ke array indeks j
+                a[j + 1] = temp;   // assign variabel yang berisi array indeks ke j ke array indeks j+1
+            }
+        }
+    }
+}
