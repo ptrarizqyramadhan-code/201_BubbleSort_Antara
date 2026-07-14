2ac202aee178e28b3e94992c5accfe6fdba41801
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-07-14 15:24:13 +0700

    cout dan void display

--- Bubbleshort.cpp
+++ Bubbleshort.cpp
@@ -17,3 +17,27 @@ void input() {
             cout << "Error : Maksimal input array adalah 20\n";
         }
     }
+
+    cout << endl; // output baris kosong
+    cout << "======================" << endl; // output ke layar
+    cout << "Masukkan elemen Array" << endl; // output ke layar
+    cout << "======================" << endl; // output ke layar
+
+    for (int i = 0; i < n; i++) // looping for dimulai dari indeks ke nol
+    {
+        cout << "Masukkan Nilai ke - " << i + 1 << " : ";
+        cin >> a[i]; // hasil input dari pengguna
+    }
+}
+
+void display() {
+    cout << endl; // baris kosong
+    cout << "=========================" << endl; // output ke layar
+    cout << "Elemen Array telah terurut" << endl; // output ke layar
+    cout << "=========================" << endl; // output ke layar
+
+    for (int i = 0; i < n; i++) // looping dimulai dari nol hingga n-1
+    {
+        cout << "Data ke - " << i << " : " << a[i] << endl; // output data array ke layar
+    }
+}
